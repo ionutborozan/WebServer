3983320b3c476965a98c9f5f33f1bd0a02e21c0d
Ionut Borozan <ionut.borozan@aciworldwide.com>
2020-11-06 18:19:35 +0200

    commig server

---
+++ src/test/java/TestWebServer.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+public class TestWebServer {
+
+    @Test
+    public void testStopServer() {
+
+    }
+
+    @Test
+    public void testScheduleMantainance() {
+
+    }
+
+    @Test
+    public void testGoToSpecifiedLocation() {
+
+    }
+
+
+}
